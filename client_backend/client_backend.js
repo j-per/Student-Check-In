90dff3886675dbd67b612ff8860ecd13acea8db6
Jesse Perdue <perdue.jesse@gmail.com>
2019-01-30 20:17:39 -0800

    styled and created functionality for table

--- client_backend/client_backend.js
+++ client_backend/client_backend.js
@@ -1,37 +1,24 @@
 const button = document.querySelector('#get_appointments');
-const output = document.querySelector('#table');
+const table = document.querySelector('#table');
 const API_URL = 'http://localhost:5000/counselorview';
 
 button.addEventListener('click', (event) => {
     fetch(API_URL)
     .then(response => response.json())
     .then(theStuff => {
-        // for(let i = 0; i < theStuff.length; i++) {
-        //     const para = document.createElement("p");
-        //     para.innerText = `${theStuff[i].studentID}`;
-        //     output.appendChild(para);
-        // }
-        const student = theStuff[theStuff.length - 1].studentID;
-        const counselor = theStuff[theStuff.length - 1].counselorName;
-        const checkInTime = theStuff[theStuff.length - 1].created;
-        structureData(student, counselor, checkInTime);
-        console.log(theStuff);
+        theStuff.forEach((currentValue, index, array) => {
+            structureData(currentValue.studentID, currentValue.counselorName, currentValue.created, index + 1);
+        })
     });
 });
 
-function structureData (student, counselor, checkin) {
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.innerText = student;
-    tr.appendChild(td);
-    tr.innerText = counselor;
-    tr.appendChild(td);
-    output.append(tr);    
+function structureData (student, counselor, checkin, rowNum) {
+    const row = table.insertRow(rowNum);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
 
+    cell1.innerText = student;
+    cell2.innerText = counselor;
+    cell3.innerText = checkin;
 }
-
-        // <tr>
-        //     <td>Alfreds Futterkiste</td>
-        //     <td>Maria Anders</td>
-        //     <td>Germany</td>
-        // </tr>
